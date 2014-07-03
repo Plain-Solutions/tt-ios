b2bb7c27e385574df3407993016755e9195aa418
Vlad Slepukhin <slp.vld@gmail.com>
2014-07-03 13:30:34 +0400

    Rewrite getting groups with GCD

--- Timetables/TTPGroupViewController.m
+++ Timetables/TTPGroupViewController.m
@@ -9,8 +9,11 @@
 #import "TTPGroupViewController.h"
 #import "TTPParser.h"
 #import "TTPTimetableViewController.h"
+
+#define ShowNetworkActivityIndicator() [UIApplication sharedApplication].networkActivityIndicatorVisible = YES
+#define HideNetworkActivityIndicator() [UIApplication sharedApplication].networkActivityIndicatorVisible = NO
+
 @interface TTPGroupViewController ()
-@property (nonatomic, strong) NSMutableData *responseData;
 @property (nonatomic, strong) TTPParser *parser;
 @property (retain) NSIndexPath* lastIndexPath;
 @end
@@ -20,7 +23,6 @@
 @synthesize groupList = _groupList;
 
 @synthesize nextButton = _nextButton;
-@synthesize responseData = _responseData;
 @synthesize parser = _parser;
 @synthesize lastIndexPath = _lastIndexPath;
 
@@ -38,14 +40,25 @@
 {
     [super viewDidLoad];
     self.title = self.selectedDepartment.name;
+	
+	dispatch_queue_t downloadQueue = dispatch_queue_create("downloader", NULL);
+    dispatch_async(downloadQueue, ^{
+		NSString *groupURL = [NSString stringWithFormat:@"http://api.ssutt.org:8080/1/department/%@/groups", self.selectedDepartment.tag];
+		ShowNetworkActivityIndicator();
+        // do our long running process here
+		NSURLRequest* request = [NSURLRequest requestWithURL: [NSURL URLWithString: groupURL] cachePolicy:NSURLRequestUseProtocolCachePolicy timeoutInterval:60];
+        NSURLResponse* response = nil;
+        NSError* error = nil;
+        NSData *data = [NSURLConnection sendSynchronousRequest:request returningResponse:&response error:&error];
 
-    self.responseData = [NSMutableData data];
-    
-    NSString *groupURLString = [NSString stringWithFormat:@"http://api.ssutt.org:8080/1/department/%@/groups", self.selectedDepartment.tag];
-    NSLog(@"%@", groupURLString);
-    NSURLRequest *request = [NSURLRequest requestWithURL:
-                             [NSURL URLWithString:groupURLString]];
-    [[NSURLConnection alloc] initWithRequest:request delegate:self];
+        // do any UI stuff on the main UI thread
+        dispatch_async(dispatch_get_main_queue(), ^{
+			self.parser = [[TTPParser alloc] init];
+			self.groupList = [self.parser parseGroups:data error:error];
+			[self.tableView reloadData];
+			HideNetworkActivityIndicator();
+        });
+    });
 
 }
 
@@ -53,30 +66,6 @@
 	[super viewWillAppear:animated];
 	[self.tableView reloadData];
 }
-
-
-- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response {
-    NSLog(@"didReceiveResponse");
-    [self.responseData setLength:0];
-}
-
-- (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data {
-    [self.responseData appendData:data];
-}
-
-- (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error {
-    NSLog(@"didFailWithError");
-}
-
-- (void)connectionDidFinishLoading:(NSURLConnection *)connection {
-    NSLog(@"connectionDidFinishLoading");
-    NSLog(@"Succeeded! Received %d bytes of data",[self.responseData length]);
-    self.parser = [[TTPParser alloc] init];
-    NSError *error;
-    self.groupList = [self.parser parseGroups:self.responseData error:error];
-   	[self.tableView reloadData];
-}
-
 
 - (void)didReceiveMemoryWarning
 {
@@ -99,7 +88,7 @@
     if (cell == nil) {
         cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:@"GroupCell"];
     }
-    NSLog(@"%@", [self.groupList objectAtIndexedSubscript:indexPath.row]);
+	
     cell.textLabel.text = [self.groupList objectAtIndexedSubscript:indexPath.row];
     if ([indexPath compare:self.lastIndexPath] == NSOrderedSame)
     {
@@ -119,46 +108,6 @@
     self.nextButton.enabled = YES;
     [tableView reloadData];
 }
-
-
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
-{
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
-{
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
 
 #pragma mark - Navigation
 
